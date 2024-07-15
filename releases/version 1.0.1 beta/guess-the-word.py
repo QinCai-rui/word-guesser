80b78e65d3e3d36ac341ef72de025e4ccac17302
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-07-16 10:03:33 +1200

    Update guess-the-word.py

--- releases/version 1.0.1 beta/guess-the-word.py
+++ releases/version 1.0.1 beta/guess-the-word.py
@@ -6,7 +6,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-# Version 1.0.1
+# Version 1.0.1 beta
 # by Raymont 'QinCai' Qin. https://qincai.lovestoblog.com
 # Visit https://qincai.lovestoblog.com/feedback/ to provide feedback
 
