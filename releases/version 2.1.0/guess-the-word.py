772446cec0b550b3fa70be51e5e1d42cb764c14a
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-11-01 18:11:54 +1300

    Update guess-the-word.py
    Edited website URL

--- releases/version 2.1.0/guess-the-word.py
+++ releases/version 2.1.0/guess-the-word.py
@@ -7,8 +7,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 """
 
 # Version 2.1.0
-# by Raymont 'QinCai' Qin. https://qincai.lovestoblog.com
-# Visit https://qincai.lovestoblog.com/feedback/ to provide feedback
+# by Raymont 'QinCai' Qin. https://qincai.xyz
+# Visit https://qincai.xyz/feedback/ to provide feedback
 # Lisenced under the GNU General Public License v3.0.
 # For more infomation, visit https://github.com/QinCai-rui/word-guesser/blob/main/LICENSE
 
@@ -162,14 +162,14 @@ def main_loop(stdscr):
         stdscr.addstr(0, 0, 'Well done, you won! Thanks for playing!')
     else:
         stdscr.addstr(0, 0, f'You died! Try again next time. The word was: {secret_word}.')
-    stdscr.addstr(1, 0, 'Visit https://qincai.lovestoblog.com/feedback/ to tell me if you discovered any issues/bugs.')
+    stdscr.addstr(1, 0, 'Visit https://qincai.xyz/feedback/ to tell me if you discovered any issues/bugs.')
     stdscr.refresh()
     time.sleep(10)
 
 if __name__ == "__main__":
     print('Hello. Welcome to Guess the Word by Raymont \'QinCai\' Qin.')
     time.sleep(2.5)
-    print('My website is at https://qincai.lovestoblog.com. Feel free to take a look at it! ')
+    print('My website is at https://qincai.xyz. Feel free to take a look at it! ')
     time.sleep(2.5)
     print('IMPORTANT: Please make sure to have Python 3 installed and to run this inside a terminal.')
     time.sleep(3)
