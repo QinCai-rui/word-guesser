6e243dea05a7742d7180f39c1fab66fe151b31a6
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-11-01 18:13:28 +1300

    Update guess-the-word.py
    Edited website URL

--- releases/version 1.0.1 beta/guess-the-word.py
+++ releases/version 1.0.1 beta/guess-the-word.py
@@ -7,8 +7,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 '''
 
 # Version 1.0.1 beta
-# by Raymont 'QinCai' Qin. https://qincai.lovestoblog.com
-# Visit https://qincai.lovestoblog.com/feedback/ to provide feedback
+# by Raymont 'QinCai' Qin. https://qincai.xyz
+# Visit https://qincai.xyz/feedback/ to provide feedback
 
 
 import curses
@@ -85,7 +85,7 @@ def main_loop(stdscr):
         stdscr.addstr(0, 0, 'Well done, you won! Thanks for playing!')
     else:
         stdscr.addstr(0, 0, f'You died! Try again next time. The word was: {secret_word}.')
-    stdscr.addstr(1, 0, 'Visit https://qincai.lovestoblog.com/feedback/ to tell me if you discovered any issues/bugs.')
+    stdscr.addstr(1, 0, 'Visit https://qincai.xyz/feedback/ to tell me if you discovered any issues/bugs.')
     stdscr.refresh()
     time.sleep(10)
 
